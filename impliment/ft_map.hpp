4b88c888ea4927a1431d529c1bc0eaa125a70e7b
Владимир Поборцев <pobortcevv@gmail.com>
2022-06-17 23:37:15 +0300

    wrap for LLRB is done, time to correct LLRB itself, also added integral

--- impliment/ft_map.hpp
+++ impliment/ft_map.hpp
@@ -24,21 +24,264 @@ namespace ft {
 		private:
 			typedef Alloc				allocator_type;
 		private:
-		// Name requirements for c++ container:
-		typedef T										value_type;
-		typedef	T&										reference;
-		typedef const T&								const_reference;
-		typedef typename allocator_type::pointer		pointer;
-		typedef typename allocator_type::const_pointer	const_pointer;
-		typedef wrap_iter<pointer>						iterator;
-		typedef wrap_iter<const_pointer>				const_iterator;
-		typedef reverse_wrap_iter<iterator>				reverse_iterator;
-		typedef reverse_wrap_iter<const_iterator>		const_reverse_iterator;
+			// Name requirements for c++ container:
+			typedef T										value_type;
+			typedef	T&										reference;
+			typedef const T&								const_reference;
+			typedef Compare         						key_compare;
+			typedef typename allocator_type::pointer		pointer;
+			typedef typename allocator_type::const_pointer	const_pointer;
+			typedef wrap_iter<pointer>						iterator;
+			typedef wrap_iter<const_pointer>				const_iterator;
+			typedef reverse_wrap_iter<iterator>				reverse_iterator;
+			typedef reverse_wrap_iter<const_iterator>		const_reverse_iterator;
 
-		typedef std::size_t size_type;
-		typedef signed int difference_type;
+			typedef std::size_t size_type;
+			typedef signed int difference_type;
 
+		// This is a function object that compares objects of type std::map::value_type (key-value pairs)
+		// by comparing of the first components of the pairs.
+		class value_compare {
+		private:
+			Compare comp;
+		public:
+			typedef typename ft::pair<Key, T> value_type;
+
+			explicit value_compare (Compare const & c) : comp(c) {}
+			virtual ~value_compare() {}
+			bool operator() (const value_type &x, const value_type &y) const {
+				return (comp(x.first, y.first));
+			}
+			bool operator() (const value_type &x, const key_type &y) const {
+				return (comp(x.first, y));
+			}
+			bool operator() (const key_type &x, const value_type &y) const {
+				return (comp(x, y.first));
+			}
+		};
+		
+		private:
+            LLRB<value_type, value_compare, allocator_type>        _map;
+
+		public:
+			typedef bool            result_type;
+			typedef value_type      first_argument_type;
+			typedef value_type      second_argument_type;
+			bool operator() (const value_type& x, const value_type& y) const {
+				return cmp(x.first, y.first);
+			}
+
+			/* ------------------------------ Constructors ------------------------------ */
+			map(const key_compare & cmp, const allocator_type& alloc): _map(LLRB<value_type, value_compare, allocator_type>(value_compare(cmp), alloc)) {}
+
+			template <class InputIterator>
+			map(InputIterator first, InputIterator last, const key_compare &comp,
+                const allocator_type &alloc,
+                typename ft::enable_if<!ft::is_integral<InputIterator>::value>::type *):
+                _map(RBTree<value_type, value_compare, allocator_type>(first, last, value_compare(comp), alloc)) {}
+
+			map(const map &x,const key_compare& comp, const allocator_type& alloc): _map(RBTree<value_type, value_compare, allocator_type>((x.begin()), (x.end()), value_compare(comp), alloc)) {}
+
+			~map() {}
+			/* -------------------------------------------------------------------------- */
+
+			/* ----------------------------- Element access ----------------------------- */
+			reference operator[](const key_type &key) {
+				iterator    it;
+				it = (find(key));
+
+				if (it == end()) {
+					_map.insert_map(ft::make_pair(key, value_type()), value_compare(key_comp()));
+					it = find(key);
+				}
+
+				return it->second;
+			}
+			/* -------------------------------------------------------------------------- */
+
+			/* -------------------------------- Iterators ------------------------------- */
+			iterator begin() { return (_map.begin()); }
+
+			const_iterator begin() const { return (_map.begin()); }
+
+			iterator end() { return (_map.end()); }
+
+			const_iterator end() const { return (_map.end()); }
+
+			reverse_iterator  rbegin() { return ((_map.rbegin())); }
+
+			const_reverse_iterator rbegin() const { return ((_map.rbegin())); }
+
+			reverse_iterator rend() { return ((_map.rend())); }
+
+			const_reverse_iterator rend() const { return ((_map.rend())); }
+
+			bool empty() const { return (_map.empty()); }
+
+			size_t size() const { return (_map.size()); }
+
+			size_t max_size() const { return (_map.max_size()); }
+			/* -------------------------------------------------------------------------- */
+
+			/* -------------------------------- Capacity -------------------------------- */
+			bool empty() const { return (_map.empty()); }
+
+			size_t size() const { return (_map.size()); }
+
+			size_t max_size() const { return (_map.max_size()); }
+			/* -------------------------------------------------------------------------- */
+
+			/* -------------------------------- Modifiers ------------------------------- */
+			pair<iterator, bool> insert(const value_type &val, const key_compare &comp, const allocator_type &alloc) {
+				if (&alloc == &alloc) {};
+				if (_map.insert_map(val, value_compare(comp))) {
+					iterator it = _map.lower_bound(val,value_compare(comp));
+					return (ft::make_pair(it, true));
+				} else {
+				iterator it = _map.lower_bound(val,value_compare(comp));
+
+				return (ft::make_pair(it, false));
+				}
+			}
+
+			template<class InputIterator>
+			void insert(InputIterator first, InputIterator last, const key_compare &comp,
+													typename ft::enable_if<!ft::is_integral<InputIterator>::value>::type *)  {
+				_map.insert(first, last, value_compare(comp));
+			}
+
+            void erase(iterator position, const key_compare& comp) {
+                _map.erase(position, value_compare(comp));
+			}
+
+			size_t erase(const key_type &k, const key_compare &cmp) {
+				iterator it = find(k);
+				if (it == end())
+					return 0;
+				_map.erase(it, value_comp(cmp));
+				return 1;
+			}
+
+			void erase(iterator first, iterator last, const key_compare &comp) {
+				_map.erase(first, last, value_compare(comp));
+			}
+
+			void swap(map &xp) {
+				_map.swap(xp._map);
+			}
+
+			void swap (map& left, map& right) {
+				left.swap(right);
+			}
+
+			void clear(const key_compare& comp) {
+				_map.clear(value_compare(comp));
+			}
+
+			key_compare key_comp(const key_compare& comp) const {
+				return comp;
+			}
+
+			value_compare value_comp(const key_compare &comp) const {
+				return value_compare(comp);
+			}
+
+			iterator find(const key_type &k, const key_compare &comp) {
+				return _map.find_map(ft::make_pair(k, mapped_type()), value_compare(comp));
+			}
+
+			const_iterator find(const key_type &k, const key_compare &comp) const {
+				return _map.find_map(ft::make_pair(k, mapped_type()), value_compare(comp));
+			}
+
+			size_t count(const key_type &k, const key_compare &comp) const {
+				return _map.count(ft::make_pair(k, mapped_type()), value_compare(comp));
+			}
+
+			iterator lower_bound(const key_type &k, const key_compare &comp) {
+				return _map.lower_bound(ft::make_pair(k, mapped_type()), value_compare(comp));
+			}
+
+			const_iterator lower_bound(const key_type &k, const key_compare &comp) const {
+				return _map.lower_bound(ft::make_pair(k, mapped_type()), value_compare(comp));
+			}
+
+			iterator upper_bound(const key_type &k, const key_compare &comp) {
+				return _map.upper_bound(ft::make_pair(k, mapped_type()), value_compare(comp));
+			}
+
+			const_iterator upper_bound(const key_type &k, const key_compare &comp) const {
+				return _map.upper_bound(ft::make_pair(k, mapped_type()), value_compare(comp));
+			}
+
+			pair<iterator, iterator> equal_range(const key_type &k, const key_compare& comp) const {
+				return make_pair(_map.lower_bound(ft::make_pair(k, mapped_type()), value_compare(comp)), _map.upper_bound(ft::make_pair(k, mapped_type()), value_compare(comp)));
+			}
+
+			allocator_type get_allocator(const allocator_type &alloc) const {
+				return alloc;
+			}
+			/* -------------------------------------------------------------------------- */
 	};
+
+	/* -------------------------------- Operators ------------------------------- */
+	template <class Key, class T, class Compare, class Alloc>
+	bool operator== (const map<Key,T,Compare,Alloc>& lhs, const map<Key,T,Compare,Alloc>& rhs ) {
+		if (lhs.size() != rhs.size())
+			return false;
+		if (lhs.size() == 0)
+			return true;
+		typename map<Key, T, Compare, Alloc>::const_iterator itL = lhs.begin();
+		typename map<Key, T, Compare, Alloc>::const_iterator itR = rhs.begin();
+		for (size_t i=0; i<lhs.size(); ++i, ++itL, ++itR) {
+			if ((itL->first != itR->first) || (itL->second != itR->second))
+				return false;
+		}
+		return true;
+	}
+
+    template <class Key, class T, class Compare, class Alloc>
+    bool operator!= (const map<Key, T, Compare, Alloc>& lhs, const map<Key, T, Compare, Alloc>& rhs) { return !(lhs == rhs); }
+
+    template <class Key, class T, class Compare, class Alloc> bool operator<  (const map<Key,T,Compare,Alloc>& lhs, const map<Key,T,Compare,Alloc>& rhs) {
+        size_t left_size = lhs.size();
+        size_t right_size = rhs.size();
+        if (left_size == 0 && right_size != 0)
+            return true;
+        else if (left_size == 0)
+            return false;
+        else if (right_size == 0)
+        	return false;
+
+        typename map<Key, T, Compare, Alloc>::const_iterator itl = lhs.begin();
+        typename map<Key, T, Compare, Alloc>::const_iterator itr = rhs.begin();
+
+        for (size_t i = 0; i < left_size && i < right_size; ++i) {
+            if (itl->first > itr->first || itl->second > itr->second)
+                return false;
+		}
+
+        if (left_size <= right_size)
+            return true;
+        else
+            return false;
+    }
+
+    template <class Key, class T, class Compare, class Alloc>
+	bool operator<= (const map<Key,T,Compare,Alloc>& lhs, const map<Key,T,Compare,Alloc>& rhs ) {
+		return ((lhs < rhs) || (lhs == rhs));
+	}
+
+    template <class Key, class T, class Compare, class Alloc>
+	bool operator> (const map<Key,T,Compare,Alloc>& lhs, const map<Key,T,Compare,Alloc>& rhs ) {
+		return (!(lhs < rhs) && !(lhs == rhs));
+	}
+
+    template <class Key, class T, class Compare, class Alloc>
+	bool operator>= (const map<Key,T,Compare,Alloc>& lhs, const map<Key,T,Compare,Alloc>& rhs) {
+		return ((lhs > rhs) || (lhs == rhs));
+	}
+	/* -------------------------------------------------------------------------- */
 }
 /* -------------------------------------------------------------------------- */
 
--- utils/utility.hpp
+++ utils/utility.hpp
@@ -2,7 +2,6 @@
 # define GENERICS_HPP
 
 namespace ft {
-
 	template<typename T>
 	void swap(T & lhs, T & rhs) {
 		T buf(rhs);
@@ -10,6 +9,8 @@ namespace ft {
 		lhs = buf;
 	}
 
+	// std::pair is a class template that provides a way to store two heterogeneous objects as a single unit.
+	// A pair is a specific case of a std::tuple with two elements.
 	template<class T1, class T2>
 	struct pair {
 		typedef T1 first_type;
@@ -20,6 +21,7 @@ namespace ft {
 
 		pair() : first(), second() {}
 		pair(first_type const &t1, second_type const &t2) : first(t1), second(t2) {}
+
 		template<class U1, class U2>
 		pair(ft::pair<U1, U2> const &p) : first(p.first), second(p.second) {} 
 		pair &operator=(pair const& p) {
@@ -38,6 +40,117 @@ namespace ft {
 	ft::pair<T1, T2> make_pair(T1 x, T2 y) {
 		return pair<T1, T2>(x, y);
 	}
+
+	// std::integral_constant wraps a static constant of specified type. It is the base class for the C++ type traits.
+	// The behavior of a program that adds specializations for integral_constant is undefined.
+	// This template is designed to provide compile-time constants as types.
+	template<typename T, T v>
+    struct integral_constant {
+        static const T                      value = v;
+        typedef T                           value_type;
+        typedef integral_constant<T, v>     type;
+        operator value_type() const { return value; }
+    };
+
+	// const volatile object - an object whose type is const-volatile-qualified, a non-mutable subobject of a const volatile object, 
+	// a const subobject of a volatile object, or a non-mutable volatile subobject of a const object. Behaves as both a const object and as a volatile object.
+	template<typename> struct remove_cv;
+
+    template<typename T> struct remove_const { typedef T	type; };
+
+    template<typename T> struct remove_const<T const> { typedef T	type; };
+
+	// Volatile is a C/C++ keyword that informs the compiler 
+	// that the value of a variable can be changed from outside and that the compiler will not optimize that variable.
+	//
+	// (Volatile — ключевое слово языков C/C++, которое информирует компилятор о том, 
+	// что значение переменной может меняться из вне и что компилятор не будет оптимизировать эту переменную).
+    template<typename T> struct remove_volatile { typedef T	type; };
+
+    template<typename T> struct remove_volatile<T volatile> { typedef T	type; };
+
+    template<typename T>
+	struct remove_cv {
+		typedef typename
+		remove_const<typename remove_volatile<T>::type>::type     type;
+	};
+
+	// The type used as a compile-time boolean with true value.
+    typedef integral_constant<bool, true> true_type;
+
+	// The type used as a compile-time boolean with false value.
+    typedef integral_constant<bool, false> false_type;
+
+    template<bool B, class T = void>
+    struct enable_if {};
+
+    template<class T>
+    struct enable_if<true, T> {
+        typedef T type;
+    };
+
+    template<typename>
+    struct is_integral_helper : public false_type {};
+
+    template<>
+    struct is_integral_helper<bool> : public true_type {};
+
+    template<>
+    struct is_integral_helper<char> : public true_type {};
+
+    template<>
+    struct is_integral_helper<signed char> : public true_type {};
+
+    template<>
+    struct is_integral_helper<unsigned char> : public true_type {};
+
+	#ifdef _GLIBCXX_USE_WCHAR_T
+    template<>
+        struct is_integral_helper<wchar_t>: public true_type { };
+	#endif
+
+    template<>
+    struct is_integral_helper<char16_t> : public true_type {};
+
+    template<>
+    struct is_integral_helper<char32_t> : public true_type {
+    };
+
+    template<>
+    struct is_integral_helper<short> : public true_type {};
+
+    template<>
+    struct is_integral_helper<unsigned short> : public true_type {};
+
+    template<>
+    struct is_integral_helper<int> : public true_type {};
+
+    template<>
+    struct is_integral_helper<unsigned int> : public true_type {};
+
+    template<>
+    struct is_integral_helper<long> : public true_type {};
+
+    template<>
+    struct is_integral_helper<unsigned long> : public true_type {};
+
+    template<>
+    struct is_integral_helper<long long> : public true_type {};
+
+    template<>
+    struct is_integral_helper<unsigned long long> : public true_type {};
+
+	#if !defined(__STRICT_ANSI__) && defined(_GLIBCXX_USE_INT128)
+    template<>
+        struct is_integral_helper<__int128>: public true_type { };
+
+    template<>
+        struct is_integral_helper<unsigned __int128>: public true_type { };
+	#endif
+
+	// Checks whether T is an integral type
+    template<typename T>
+	struct is_integral : public integral_constant<bool, (is_integral_helper<typename ft::remove_cv<T>::type>::value)> {};
 }
 
 #endif
